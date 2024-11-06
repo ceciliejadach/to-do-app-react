7609d372243c71f3a3194ae5f19d7ecb0cbc747f
Cecilie <cepe0001@stud.kea.dk>
2024-11-06 09:15:02 +0100

    fået add task og remove task til at fungere

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import ToDoApp from "@/components/ToDoApp";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <ToDoApp />
+    </div>
+  );
 }
--- src/components/ToDoApp.jsx
+++ src/components/ToDoApp.jsx
@@ -1,5 +1,54 @@
+"use client";
+import { useState } from "react";
+
 const ToDoApp = () => {
-  return;
+  const [tasks, setTasks] = useState([]);
+
+  function addTask(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const task = formData.get("task");
+
+    const newTask = {
+      id: crypto.randomUUID(),
+      text: task,
+      completed: false,
+    };
+    setTasks(tasks.concat(newTask));
+
+    event.target.reset();
+  }
+
+  return (
+    <section>
+      <form onSubmit={addTask} action="">
+        <label htmlFor="task">Write your task:</label>
+        <input type="text" name="task" id="task" />
+        <button
+          onClick={() => {
+            setTasks([...tasks]);
+          }}
+          type="submit"
+        >
+          Add task
+        </button>
+      </form>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.text}{" "}
+            <button
+              onClick={() => {
+                setTasks(tasks.filter((a) => a.id !== task.id));
+              }}
+            >
+              Remove
+            </button>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 export default ToDoApp;
